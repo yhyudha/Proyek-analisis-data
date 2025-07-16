bde603b68116651239d8bc97eb7be7ecc24aefcb
yhyudha <yohannes.h.yudha@gmail.com>
2025-07-16 21:42:42 +0700

    Update dashboard2.py

--- dashboard/dashboard2.py
+++ dashboard/dashboard2.py
@@ -1,1 +1,357 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
 
+
+# Mount Google Drive
+from google.colab import drive
+drive.mount('/content/drive')
+
+# Path folder data di Google Drive Anda
+data_dir = '/content/drive/MyDrive/Colab Notebooks/Tugas YH Yudha - Proyek Analisis Data/data/'
+
+# Daftar nama file csv yang ingin dibaca
+csv_files = [
+    'order_payments_dataset.csv',
+    'order_items_dataset.csv',
+    'orders_dataset.csv',
+    'order_reviews_dataset.csv',
+    'sellers_dataset.csv',
+    'customers_dataset.csv',
+    'product_category_name_translation.csv',
+    'products_dataset.csv'
+]
+
+# Membaca semua file dan simpan ke dictionary
+import pandas as pd
+dfs = {}
+
+for file in csv_files:
+    file_path = data_dir + file
+    try:
+        df = pd.read_csv(file_path)
+        dfs[file] = df
+        print(f"✅ Berhasil memuat {file} dari path: {file_path}")
+    except FileNotFoundError:
+        print(f"❌ Error: File {file} tidak ditemukan di path: {file_path}")
+    except Exception as e:
+        print(f"❌ Terjadi error lain saat memuat {file}: {e}")
+
+# Contoh akses dataframe
+order_payments_dataset_df = dfs['order_payments_dataset.csv']
+order_items_dataset_df = dfs['order_items_dataset.csv']
+orders_dataset_df = dfs['orders_dataset.csv']
+order_reviews_dataset = dfs['order_reviews_dataset.csv']
+sellers_dataset_df = dfs['sellers_dataset.csv']
+customers_dataset_df = dfs['customers_dataset.csv']
+product_category_name_translation_df = dfs['product_category_name_translation.csv']
+products_dataset_df = dfs['products_dataset.csv']
+# Cek head
+print(orders_dataset_df.head())
+
+
+print("Menilai tipe data :")
+print()
+print(orders_dataset_df.info())
+print()
+print("Menilai missing value :")
+print()
+print(orders_dataset_df.isna().sum())
+print()
+print("Jumlah Duplikasi : ")
+print()
+print(orders_dataset_df.duplicated().sum())
+print()
+print("Menilai Parameter Statistik :")
+print()
+print(orders_dataset_df.describe())
+print()
+
+# Cleaning dataset
+## Mengisi Tanggal yang kosong dengan NaT
+print(orders_dataset_df[orders_dataset_df.order_approved_at.isna()])
+orders_dataset_df.info()
+cols = [
+    "order_approved_at",
+    "order_delivered_carrier_date",
+    "order_delivered_customer_date",
+    "order_estimated_delivery_date"
+]
+for col in cols:
+    orders_dataset_df[col] = orders_dataset_df[col].fillna(pd.NaT)
+print(orders_dataset_df[orders_dataset_df.order_approved_at.isna()])
+orders_dataset_df.info()
+
+## Rubah fomat datetime
+datetime_columns = ["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date"]
+for column in datetime_columns:
+    orders_dataset_df[column] = pd.to_datetime(orders_dataset_df[column])
+orders_dataset_df.info()
+
+
+
+# Mengambil tanggal terawal (min()) dan terakhir (max()) dari kolom order_purchase_timestamp
+# Akan digunakan sebagai batas awal dan akhir pada input tanggal.
+min_date = orders_dataset_df["order_purchase_timestamp"].min()
+max_date = orders_dataset_df["order_purchase_timestamp"].max()
+
+
+# Mengaktifkan sidebar di Streamlit (semua kode di bawah with st.sidebar: akan muncul di sidebar).
+# Menampilkan gambar/logo di sidebar (ambil dari link GitHub).
+with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/yhyudha/analisis-data/blob/main/data/bitcoin-4130299_640.png?raw=true")
+
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Pilih Tanggal', min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+# Memfilter baris dalam DataFrame hanya pada rentang tanggal yang dipilih user pada input tadi.
+orders_dataset_df = orders_dataset_df[
+    (orders_dataset_df["order_purchase_timestamp"] >= str(start_date)) &
+    (orders_dataset_df["order_purchase_timestamp"] <= str(end_date))
+    ]
+
+# # dataset-Order Harian
+order_payments_df = pd.merge(orders_dataset_df, order_payments_dataset_df, on="order_id", how="inner")
+
+# # dataset-Product performance
+order_product_df = pd.merge(order_items_dataset_df, orders_dataset_df, on="order_id", how="inner")
+order_product_df = pd.merge(order_product_df, products_dataset_df, on="product_id", how="inner")
+order_product_df = pd.merge(order_product_df, product_category_name_translation_df, on="product_category_name", how="left")
+
+# # dataset-Demography
+order_customer_df = pd.merge(order_items_dataset_df, orders_dataset_df, on="order_id", how="inner")
+order_customer_df = pd.merge(order_customer_df, customers_dataset_df, on="customer_id", how="inner")
+
+# # dataset-RFM
+customer_order_df = pd.merge(customers_dataset_df, orders_dataset_df, on="customer_id", how="inner")
+customer_order_df = pd.merge(customer_order_df, order_items_dataset_df, on="order_id", how="left")
+
+# Helper function yang dibutuhkan untuk menyiapkan berbagai dataframe
+
+## dataset-Product performance
+
+### Menghitung jumlah order dan total revenue (pendapatan) per hari.
+def create_daily_orders_df(df):
+    daily_orders_df = df.resample(rule='D', on='order_purchase_timestamp').agg({
+        "order_id": "nunique",
+        "total_price": "sum"
+    }).reset_index()
+
+    daily_orders_df.rename(columns={
+        "order_id": "order_count",
+        "total_price": "revenue"
+    }, inplace=True)
+    return daily_orders_df
+
+### Mendapatkan total order per kategori produk.
+def create_sum_order_items_df(df):
+    sum_order_items_df = (
+        df.groupby("product_category_name_english")["order_id"]
+        .nunique()
+        .sort_values(ascending=False)
+        .reset_index(name="total_orders")
+    )
+    return sum_order_items_df
+
+### Mengetahui jumlah customer unik per state/daerah.
+def create_bystate_df(df):
+    bystate_df = df.groupby(by="customer_state").customer_id.nunique().reset_index()
+    bystate_df.rename(columns={"customer_id": "customer_count"}, inplace=True)
+    return bystate_df
+
+### Analisis RFM (Recency, Frequency, Monetary) per customer.
+def create_rfm_df(df):
+    df["order_purchase_timestamp"] = pd.to_datetime(df["order_purchase_timestamp"])
+    recent_date = df["order_purchase_timestamp"].max().date()
+    rfm_df = df.groupby("customer_id", as_index=False).agg({
+        "order_purchase_timestamp": "max",
+        "order_id": "nunique",
+        "price": "sum"
+    })
+    rfm_df.columns = ["customer_id", "last_purchase_date", "frequency", "monetary"]
+    rfm_df["last_purchase_date"] = rfm_df["last_purchase_date"].dt.date
+    rfm_df["recency"] = rfm_df["last_purchase_date"].apply(lambda x: (recent_date - x).days)
+    rfm_df.drop("last_purchase_date", axis=1, inplace=True)
+    return rfm_df
+
+
+## Menyiapkan Dataframe Analitik
+### DataFrame untuk total order per kategori produk.
+sum_order_items_df = create_sum_order_items_df(order_product_df)
+
+### DataFrame untuk jumlah customer per state.
+bystate_df = create_bystate_df(order_customer_df)
+
+### DataFrame untuk analisis RFM per customer.
+rfm_df = create_rfm_df(customer_order_df)
+
+# Bikin kolom total_price dari payment_value
+order_payments_df["total_price"] = order_payments_df["payment_value"]
+
+# Panggil fungsi untuk dapatkan daily_orders_df
+daily_orders_df = create_daily_orders_df(order_payments_df)
+# Display Daily
+st.subheader('Daily Orders')
+col1, col2 = st.columns(2)
+
+with col1:
+    total_orders = daily_orders_df["order_count"].sum()
+    st.metric("Total orders", value=total_orders)
+
+with col2:
+    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "AUD", locale='es_CO')
+    st.metric("Total Revenue", value=total_revenue)
+
+
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_purchase_timestamp"],  # X-axis
+    daily_orders_df["order_count"],              # Y-axis
+    marker='o',
+    linewidth=2
+)
+ax.set_title("Daily Orders Over Time")
+ax.set_xlabel("Tanggal")
+ax.set_ylabel("Jumlah Order")
+st.pyplot(fig)
+
+# Display Performance
+st.subheader("Product Performance Overview")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20, 5), dpi=100)
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+# # Best Performing
+sns.barplot(
+    x="total_orders",
+    y="product_category_name_english",
+    data=sum_order_items_df.head(5),
+    hue="product_category_name_english",
+    palette=colors,
+    ax=ax[0],
+    legend=False
+)
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Orders", fontsize=12)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=14)
+ax[0].tick_params(axis='y', labelsize=10)
+ax[0].tick_params(axis='x', labelsize=10)
+
+# # Worst Performing
+sns.barplot(
+    x="total_orders",
+    y="product_category_name_english",
+    data=sum_order_items_df.sort_values(by="total_orders", ascending=True).head(5),
+    hue="product_category_name_english",
+    palette=colors,
+    ax=ax[1],
+    legend=False
+)
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Orders", fontsize=12)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=14)
+ax[1].tick_params(axis='y', labelsize=10)
+ax[1].tick_params(axis='x', labelsize=10)
+st.pyplot(fig)
+
+# Display Demography
+# # Sort and create colors
+st.subheader("Customer Demography by State")
+
+sorted_df = bystate_df.sort_values(by="customer_count", ascending=False)
+colors = ["#90CAF9" if i == 0 else "#D3D3D3" for i in range(len(sorted_df))]
+
+# # Plotting
+fig, ax = plt.subplots(figsize=(20, 10))
+sns.barplot(
+    x="customer_count",
+    y="customer_state",
+    hue="customer_state",
+    data=sorted_df,
+    palette=colors,
+    ax=ax
+)
+ax.set_title("Number of Customers by State", loc="center", fontsize=30)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+st.pyplot(fig)
+
+# Display RFM
+st.subheader("Best Customer Based on RFM Parameters")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    avg_recency = round(rfm_df.recency.mean(), 1)
+    st.metric("Average Recency (days)", value=avg_recency)
+
+with col2:
+    avg_frequency = round(rfm_df.frequency.mean(), 2)
+    st.metric("Average Frequency", value=avg_frequency)
+
+with col3:
+    avg_monetary = format_currency(rfm_df.monetary.mean(), "USD", locale='id_ID')
+    st.metric("Average Monetary", value=avg_monetary)
+
+# # Visualisasi Top Customers
+fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(35, 15))
+colors = ["#90CAF9"] * 5
+
+sns.barplot(
+    y="recency",
+    x="customer_id",
+    hue="customer_id",
+    data=rfm_df.sort_values(by="recency", ascending=True).head(5),
+    palette=colors,
+    ax=ax[0],
+    legend=False
+)
+ax[0].set_title("By Recency (days)", loc="center", fontsize=50)
+ax[0].set_xlabel("customer_id", fontsize=30)
+ax[0].tick_params(axis='x', labelsize=35)
+ax[0].tick_params(axis='y', labelsize=30)
+ax[0].tick_params(axis='x', labelrotation=85)
+sns.barplot(
+    y="frequency",
+    x="customer_id",
+    hue="customer_id",
+    data=rfm_df.sort_values(by="frequency", ascending=False).head(5),
+    palette=colors,
+    ax=ax[1],
+    legend=False
+)
+ax[1].set_title("By Frequency", loc="center", fontsize=50)
+ax[1].set_xlabel("customer_id", fontsize=30)
+ax[1].tick_params(axis='x', labelsize=35)
+ax[1].tick_params(axis='y', labelsize=30)
+ax[1].tick_params(axis='x', labelrotation=85)
+
+sns.barplot(
+    y="monetary",
+    x="customer_id",
+    hue="customer_id",
+    data=rfm_df.sort_values(by="monetary", ascending=False).head(5),
+    palette=colors,
+    ax=ax[2],
+    legend=False
+)
+ax[2].set_title("By Monetary", loc="center", fontsize=50)
+ax[2].set_xlabel("customer_id", fontsize=30)
+ax[2].tick_params(axis='x', labelsize=35)
+ax[2].tick_params(axis='y', labelsize=30)
+ax[2].tick_params(axis='x', labelrotation=85)
+
+st.pyplot(fig)
+print("Current working directory:", os.getcwd())
+
+st.caption('Copyright © LASKAR AI 2025')
